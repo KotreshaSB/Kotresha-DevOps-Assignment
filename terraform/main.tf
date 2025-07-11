087c39fd7ff41e739e0b6c0758bca332136a7e77
KOTRESHA <apple@Kotresha.local>
2025-07-12 01:15:37 +0530

    created folder structure again

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,25 +1,28 @@
-terraform {
-  required_version = ">= 1.0.0"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = var.aws_region
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  name    = "kotresha-vpc"
+  cidr    = "10.0.0.0/16"
+  azs     = ["ap-south-1a", "ap-south-1b"]
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
+  enable_nat_gateway = true
+  single_nat_gateway = true
 }
 
 module "eks" {
-  source          = "./eks"
+  source          = "terraform-aws-modules/eks/aws"
   cluster_name    = var.cluster_name
-  cluster_version = var.cluster_version
-  vpc_id          = var.vpc_id
-  subnets         = var.subnets
+  cluster_version = "1.27"
+  subnets         = module.vpc.private_subnets
+  vpc_id          = module.vpc.vpc_id
+  enable_irsa     = true
+
+  node_groups = {
+    default = {
+      desired_capacity = 2
+      max_capacity     = 3
+      min_capacity     = 1
+      instance_types   = ["t3.medium"]
+    }
+  }
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,29 +1,7 @@
-variable "aws_region" {
-  description = "AWS region"
-  type        = string
-  default     = "us-east-1"
-}
-
 variable "cluster_name" {
-  description = "EKS cluster name"
-  type        = string
-  default     = "appscrip-devops-cluster"
+  default = "kotresha-eks"
 }
 
-variable "cluster_version" {
-  description = "Kubernetes version"
-  type        = string
-  default     = "1.27"
-}
-
-variable "vpc_id" {
-  description = "VPC ID where EKS will be deployed"
-  type        = string
-  default     = "" # Leave empty to create new VPC
-}
-
-variable "subnets" {
-  description = "List of subnet IDs"
-  type        = list(string)
-  default     = [] # Leave empty to create new subnets
+variable "region" {
+  default = "ap-south-1"
 }
